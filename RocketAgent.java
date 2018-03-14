48ab7f8f8cb8922861357aec87a3a769719b7e9b
Jozef Lelic <joseph.lelic@gmail.com>
2018-03-14 17:20:29 +0100

    Improve rocket logic

--- RocketAgent.java
+++ RocketAgent.java
@@ -3,8 +3,17 @@ import bc.*;
 public class RocketAgent extends UnitAgent {
     public int nInside = 0;
 
+    long marsWidth;
+    long marsHeight;
+
+    // how many turns rocket waited without anyone entering
+    public int turnsWaiting = 0;
+    public final int MAX_WAIT_FOR_TURNS = 100;
+
     public RocketAgent(int id, Central central) {
         super(id, central);
+        marsHeight = central.gc.startingMap(Planet.Mars).getHeight();
+        marsWidth = central.gc.startingMap(Planet.Mars).getWidth();
     }
 
     public void prepareTurn() {}
@@ -16,16 +25,53 @@ public class RocketAgent extends UnitAgent {
                     central.gc.unload(id, Direction.values()[i]);
         }
         else {
-            if(central.gc.unit(id).structureMaxCapacity() == nInside || central.gc.unit(id).health() < 10 || central.turnNumber > 725) {
+            turnsWaiting++;
+            if(central.gc.unit(id).structureMaxCapacity() == nInside
+                    || central.gc.unit(id).health() < 10
+                    || central.turnNumber > 725
+                    || (nInside > 0 & turnsWaiting > MAX_WAIT_FOR_TURNS)
+                    ) {
                 MapLocation landingLoc;
+                int locationsTried = 0;
+                int minPassableNeighbours;
                 do {
-                    landingLoc = new MapLocation(Planet.Mars,
-                            central.rng.nextInt((int)central.gc.startingMap(Planet.Mars).getWidth()),
-                            central.rng.nextInt((int)central.gc.startingMap(Planet.Mars).getHeight()));
+                    int x = central.rng.nextInt((int)central.gc.startingMap(Planet.Mars).getWidth());
+                    int y = central.rng.nextInt((int)central.gc.startingMap(Planet.Mars).getHeight());
+                    landingLoc = new MapLocation(Planet.Mars, x, y);
+                    locationsTried++;
+                    minPassableNeighbours = 8 - locationsTried/10;
+                    if(getPassableNeighboursCount(x,y, central) < minPassableNeighbours) {
+                        continue;
+                    }
                 } while(central.gc.startingMap(Planet.Mars).isPassableTerrainAt(landingLoc) == 0);
-                if(central.gc.canLaunchRocket(id, landingLoc))
+                if(central.gc.canLaunchRocket(id, landingLoc)) {
                     central.gc.launchRocket(id, landingLoc);
+                    turnsWaiting = 0;
+                }
             }
         }
+    }
+
+    private int getPassableNeighboursCount(int x, int y, Central central){
+        int result = 0;
+        MapLocation neighboringLocation = new MapLocation(Planet.Mars, x, y);
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                if(i == 0 && j == 0) {
+                    continue;
+                }
+                int nx = x + i;
+                int ny = y + j;
+                if (nx < 0 || ny < 0 || nx >= marsWidth || ny >= marsHeight) {
+                    continue;
+                }
+                neighboringLocation.setX(nx);
+                neighboringLocation.setY(ny);
+                if(central.gc.startingMap(Planet.Mars).isPassableTerrainAt(neighboringLocation) != 0){
+                    result++;
+                }
+            }
+        }
+        return result;
     }
 }
